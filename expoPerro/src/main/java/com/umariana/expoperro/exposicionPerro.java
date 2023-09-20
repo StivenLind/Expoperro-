632efa6e7feddeaeb3b20d43302c48a6a3194b4a
StivenLind <125386504+StivenLind@users.noreply.github.com>
2023-09-20 17:31:09 -0500

    Add files via upload

---
+++ expoPerro/src/main/java/com/umariana/expoperro/exposicionPerro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umariana.expoperro;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Posgrados
+ */
+public class exposicionPerro {
+    
+    
+   ArrayList <perro> darPerros = new ArrayList<perro>();
+   
+   
+    
+}
---
+++ expoPerro/src/main/java/com/umariana/expoperro/perro.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.umariana.expoperro;
+
+/**
+ *
+ * @author Posgrados
+ */
+public class perro {
+    // Creamos variables de las caracteristicas que va a llevar el perro
+    String nombre;
+    String raza;
+    String imagen;
+    int puntos;
+    int edad;
+
+     //método constructor vacio
+    public perro() {
+        
+    }
+    //método costructor lleno
+    public perro(String nombre, String raza, String imagen, int puntos, int edad) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.imagen = imagen;
+        this.puntos = puntos;
+        this.edad = edad;
+    }
+    
+    //setters y getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+     
+    
+}
